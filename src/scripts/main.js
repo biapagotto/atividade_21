4eaba6e5a5d5631d81dfabd8ce765d59b1b9cc38
biapagotto <biapagotto.ux@gmail.com>
2024-08-05 17:10:21 -0300

    Atualização 8

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
-    const questions = document.querySelectorAll ('[data-faq-question]');
-
     const heroSection = document.querySelector('.hero');
     const alturaHero = heroSection.clientHeight;
 
@@ -13,5 +11,40 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             exibeElementosDoHeader();
         }
-    })
-})
+    });
+
+    // Seção de atrações, programação das abas
+    buttons.forEach(button => {
+        button.addEventListener('click', function(event) {
+            const abaAlvo = event.target.dataset.tabButton;
+            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
+            escondeTodasAbas();
+            aba.classList.add('shows__list--is-active');
+            removeBotaoAtivo();
+            event.target.classList.add('shows__tabs__button--is-active');
+        });
+    });
+
+    function ocultaElementoDoHeader() {
+        const header = document.querySelector('header');
+        header.classList.add('header--is-hidden');
+    }
+
+    function exibeElementosDoHeader() {
+        const header = document.querySelector('header');
+        header.classList.remove('header--is-hidden');
+    }
+
+    function removeBotaoAtivo() {
+        buttons.forEach(button => {
+            button.classList.remove('shows__tabs__button--is-active');
+        });
+    }
+
+    function escondeTodasAbas() {
+        const tabsContainer = document.querySelectorAll('[data-tab-id]');
+        tabsContainer.forEach(tab => {
+            tab.classList.remove('shows__list--is-active');
+        });
+    }
+});
